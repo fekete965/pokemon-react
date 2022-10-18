243bfe186e6de1d8c13cfc2641859b98706b5fd7
Bence Fekete <fekete965@gmail.com>
2022-10-18 22:39:35 +0100

    Add paths for vite prettier and tsconfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'node:path'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  resolve: {
+    alias: {
+      api: path.resolve(__dirname, 'api'),
+      models: path.resolve(__dirname, 'models'),
+    },
+  },
 })
