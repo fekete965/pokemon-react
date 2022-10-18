2f9f373644b6bc17fe0092c1bd0e2e2c70027a2d
Bence Fekete <fekete965@gmail.com>
2022-10-18 23:41:28 +0100

    Add NotFoundPage

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom'
 
 import Home from './screens/Home'
+import NotFound from './screens/NotFound'
 import Pokemon from './screens/Pokemon'
 
 export const router = createBrowserRouter([
@@ -11,5 +12,9 @@ export const router = createBrowserRouter([
   {
     path: 'pokemons/:pokemonId',
     element: <Pokemon />,
+  },
+  {
+    path: '*',
+    element: <NotFound />,
   },
 ])
