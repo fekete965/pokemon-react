9fb07a366846dd5bc954a4887e936f168aef0c21
Bence Fekete <fekete965@gmail.com>
2022-10-18 18:23:06 +0100

    Setup initial project

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div className="text-3xl font-bold underline">Pokemon App</div>
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import App from './App'
+import './styles/app.css'
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
