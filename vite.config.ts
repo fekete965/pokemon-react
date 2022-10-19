cbb630a1d55a639ec5636a48f43c0a009b54ea92
Bence Fekete <fekete965@gmail.com>
2022-10-19 23:06:44 +0100

    Add context folder to aliases

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
     alias: {
       api: path.resolve(__dirname, 'api'),
       assets: path.resolve(__dirname, 'src', 'assets'),
+      context: path.resolve(__dirname, 'src', 'context'),
       components: path.resolve(__dirname, 'src', 'components'),
       models: path.resolve(__dirname, 'models'),
       routes: path.resolve(__dirname, 'src', 'routes'),
