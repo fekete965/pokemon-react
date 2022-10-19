78ef389c23e503ed2051d4561dcdcaec6064d237
Bence Fekete <fekete965@gmail.com>
2022-10-19 21:05:45 +0100

    Add more aliases to tsconfig and vite

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,12 @@ export default defineConfig({
   resolve: {
     alias: {
       api: path.resolve(__dirname, 'api'),
+      assets: path.resolve(__dirname, 'src', 'assets'),
+      components: path.resolve(__dirname, 'src', 'components'),
       models: path.resolve(__dirname, 'models'),
+      routes: path.resolve(__dirname, 'src', 'routes'),
+      styles: path.resolve(__dirname, 'styles'),
+      utils: path.resolve(__dirname, 'utils'),
     },
   },
 })
