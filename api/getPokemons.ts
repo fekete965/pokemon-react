b843f5f9f04fcdc377a38f2e436bb42a977e63bf
Bence Fekete <fekete965@gmail.com>
2022-10-19 14:10:50 +0100

    Add getPokemonBy and update getPokemons requests

--- api/getPokemons.ts
+++ api/getPokemons.ts
@@ -1,7 +1,7 @@
 import { ReferenceResourceResponse } from 'models/ReferenceResource'
 
-export const getPokemons = (nameOrId?: string): Promise<ReferenceResourceResponse> => {
-  return fetch(`https://pokeapi.co/api/v2/pokemon${nameOrId ? '/' + nameOrId : ''}`).then(resp => resp.json())
+export const getPokemons = (): Promise<ReferenceResourceResponse> => {
+  return fetch('https://pokeapi.co/api/v2/pokemon').then(resp => resp.json())
 }
 
 export default getPokemons
